ac2c00a7fa1dfe572dcd7f2a37fa3ee3c8c4afd4
Samuel <87152786+bigDegen@users.noreply.github.com>
2023-04-06 20:53:37 +0200

    added overflow test
    Added test to see if server works if lobby gets overrun by players

--- app/src/test/java/com/example/lottikarotti/network/Test_Network.java
+++ app/src/test/java/com/example/lottikarotti/network/Test_Network.java
@@ -11,6 +11,12 @@ import io.socket.client.Socket;
 public class Test_Network {
 
     static Socket testSocket01;
+    static Socket testSocket02;
+    static Socket testSocket03;
+    static Socket testSocket04;
+    static Socket testSocket05;
+    static Socket testSocket06;
+    static Socket testSocket07;
     static int aliveFeedback;
 
     /**
@@ -53,6 +59,44 @@ public class Test_Network {
 
         // Check if feedback value is set to 1
         Assertions.assertEquals(1, aliveFeedback);
+    }
+    @Test
+    void testAdvancedServerOverflow(){
+        Assertions.assertDoesNotThrow(()->{
+            testSocket02 = IO.socket("http://127.0.0.1:3000");
+            testSocket02.connect();
+
+            testSocket03 = IO.socket("http://127.0.0.1:3000");
+            testSocket03.connect();
+
+            testSocket04 = IO.socket("http://127.0.0.1:3000");
+            testSocket04.connect();
+
+            testSocket05 = IO.socket("http://127.0.0.1:3000");
+            testSocket05.connect();
+
+            testSocket06 = IO.socket("http://127.0.0.1:3000");
+            testSocket06.connect();
+        });
+
+        testSocket06.on("alive", args -> {aliveFeedback = (Integer) args[0];});
+
+        // Initialize feedback value
+        aliveFeedback = 99;
+
+        // Emit "alive" event to the server
+        testSocket06.emit("alive");
+
+        // Wait for the server to respond (MAX TIME IS 2 SECOND; The call is made synchronous on purpose to detect high latency!)
+        // Please Check Internet Connection before testing, the internet connection should not be stressed when testing
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Check if feedback value is set to 1
+        Assertions.assertEquals(99, aliveFeedback);
     }
 
 
