d4c9fea8fa783d480c4ef34cedb83c7204f14af5
sveblu <svbluem@edu.aau.at>
2023-05-03 20:11:24 +0200

    Created a SettingsActivityTest class for tests.

---
+++ app/src/test/java/com/example/lottikarotti/settings/SettingsActivityTest.java
@@ -0,0 +1,27 @@
+package com.example.lottikarotti.settings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.lottikarotti.SettingsActivity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SettingsActivityTest {
+
+    SettingsActivity settingsActivity;
+
+    @BeforeEach
+    void setUp() {
+        settingsActivity = new SettingsActivity();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void onCreate() {
+    }
+}
