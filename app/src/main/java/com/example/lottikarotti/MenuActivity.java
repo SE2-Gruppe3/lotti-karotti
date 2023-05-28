818948a9af1367cd991cde3ff05f84b2437f97d8
Samuel <87152786+syntaxsavr@users.noreply.github.com>
2023-05-28 20:40:25 +0200

    update import

--- app/src/main/java/com/example/lottikarotti/MenuActivity.java
+++ app/src/main/java/com/example/lottikarotti/MenuActivity.java
@@ -9,11 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.SeekBar;
-import android.widget.Switch;
 
 import com.example.lottikarotti.Highscore.HighscoreActivity;
+import com.example.lottikarotti.Util.BGMusic;
 
 public class MenuActivity extends AppCompatActivity {
 
