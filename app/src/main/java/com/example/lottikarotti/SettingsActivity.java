fbbc69db57450068e15f78824e894bb501f2be3d
Samuel <87152786+syntaxsavr@users.noreply.github.com>
2023-05-28 20:40:17 +0200

    update import

--- app/src/main/java/com/example/lottikarotti/SettingsActivity.java
+++ app/src/main/java/com/example/lottikarotti/SettingsActivity.java
@@ -17,6 +17,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 
+import com.example.lottikarotti.Util.BGMusic;
+
 public class SettingsActivity extends AppCompatActivity {
 
     //**Relevant variables for Volume**//
