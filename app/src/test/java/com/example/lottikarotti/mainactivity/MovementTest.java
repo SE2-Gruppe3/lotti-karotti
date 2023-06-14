6547c99cc015226d1afd79ca9284939e057ebcc5
Samuel <87152786+syntaxsavr@users.noreply.github.com>
2023-06-15 00:09:30 +0200

    Add Test

--- app/src/test/java/com/example/lottikarotti/mainactivity/MovementTest.java
+++ app/src/test/java/com/example/lottikarotti/mainactivity/MovementTest.java
@@ -1,64 +1,55 @@
-import static org.mockito.ArgumentMatchers.*;
+package com.example.lottikarotti.mainactivity;
+
 import static org.mockito.Mockito.*;
-
-import android.app.Activity;
-import android.view.View;
-
-import com.example.lottikarotti.MainActivity;
+import com.example.lottikarotti.GameLogic.PlayerMove;
 import com.example.lottikarotti.Network.ServerConnection;
+import com.example.lottikarotti.Player;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.MockedStatic;
-import org.mockito.junit.jupiter.MockitoExtension;
-import io.socket.client.Socket;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MovementTest {
+
     private MockedStatic<ServerConnection> mockedServerConnection;
-    private Socket mockedSocket;
-    private MainActivity main;
-    private View viewMock;
 
     @BeforeEach
     public void setup() {
-        main = new MainActivity();
-
-        mockedSocket = mock(Socket.class);
+    // Mock the ServerConnection class
         mockedServerConnection = mockStatic(ServerConnection.class);
-        mockedServerConnection.when(() -> ServerConnection.getInstance(anyString()))
-                .thenReturn(mockedSocket);
-
-        viewMock = mock(View.class);
     }
 
     @AfterEach
     public void teardown() {
+        // Close the mocked ServerConnection
         mockedServerConnection.close();
     }
 
-    @Test
-    public void testServerConnection() {
-        // Perform test on the server connection
-        ServerConnection.connect();
-        when(mockedSocket.connected()).thenReturn(true);
-        Assertions.assertTrue(mockedSocket.connected());
-    }
 
     @Test
-    public void testMoveListener() {
-        int steps = 5;
-        main.selectRabbit(1);
-        main.drawButtonListener(viewMock);
+    public void testIncomingMovement() throws JsonProcessingException {
+        String json = "[{\"sid\":\"LPRTFbJ099djPWfZAAAN\",\"lobbycode\":\"732836\",\"color\":\"white\",\"rabbits\":[{\"name\":\"rabbit1\",\"position\":0},{\"name\":\"rabbit2\",\"position\":0},{\"name\":\"rabbit3\",\"position\":0},{\"name\":\"rabbit4\",\"position\":0}]}]";
 
-        //Test if drawing worked
-        Assertions.assertEquals(1, main.drawbut
+        Assertions.assertThrows(JsonProcessingException.class, () -> {
+            PlayerMove.handleMoveFromServer("abc");
+        });
 
+        Assertions.assertDoesNotThrow(() -> {
+            PlayerMove.handleMoveFromServer(json);
+        });
+
+        List<Player> players = new ArrayList<Player>();
+        players.add(new Player("LPRTFbJ099djPWfZAAAN", "732836", "white", null));
+
+        Assertions.assertNotEquals(PlayerMove.handleMoveFromServer(json).toArray().toString(), players.toArray().toString());
     }
-
-    // Add more test methods for other server listeners following a similar structure
 }
+
