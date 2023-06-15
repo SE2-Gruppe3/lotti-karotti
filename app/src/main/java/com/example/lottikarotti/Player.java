8a907a55a6630ef1a018bbce442ca65570eaad02
Samuel <87152786+syntaxsavr@users.noreply.github.com>
2023-06-15 13:25:58 +0200

    Update Player.java

--- app/src/main/java/com/example/lottikarotti/Player.java
+++ app/src/main/java/com/example/lottikarotti/Player.java
@@ -70,51 +70,54 @@ public class Player {
     public List<Rabbit> getRabbits() {
         return rabbits;
     }
-}
-
-/**
- * This class represents a Rabbit object in the Lotti Karotti game.
- */
-class Rabbit {
-
-    // Instance variables
-    private String name; // Name of the rabbit
-    private int position; // Position of the rabbit on the game board
-
-
-
-    /**
-     Default constructor for Rabbit class.
-     */
-    public Rabbit() {
-    }
-
-    /**
-     Constructor for Rabbit class.
-     @param name the name of the rabbit.
-     @param position the position of the rabbit on the game board.
-     */
-    @JsonCreator
-    public Rabbit(@JsonProperty("name") String name, @JsonProperty("position") int position) {
-        this.name = name;
-        this.position = position;
-    }
-
-    /**
-     Getter for rabbit name.
-     @return the name of the rabbit.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     Getter for rabbit position.
-     @return the position of the rabbit on the game board.
-     */
-    public int getPosition() {
-        return position;
-    }
 
 }
+    /**
+     * This class represents a Rabbit object in the Lotti Karotti game.
+     */
+    class Rabbit {
+
+        // Instance variables
+        private String name; // Name of the rabbit
+        private int position; // Position of the rabbit on the game board
+
+
+        /**
+         * Default constructor for Rabbit class.
+         */
+        public Rabbit() {
+        }
+
+        /**
+         * Constructor for Rabbit class.
+         *
+         * @param name     the name of the rabbit.
+         * @param position the position of the rabbit on the game board.
+         */
+        @JsonCreator
+        public Rabbit(@JsonProperty("name") String name, @JsonProperty("position") int position) {
+            this.name = name;
+            this.position = position;
+        }
+
+        /**
+         * Getter for rabbit name.
+         *
+         * @return the name of the rabbit.
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * Getter for rabbit position.
+         *
+         * @return the position of the rabbit on the game board.
+         */
+        public int getPosition() {
+            return position;
+        }
+
+    }
+
 
