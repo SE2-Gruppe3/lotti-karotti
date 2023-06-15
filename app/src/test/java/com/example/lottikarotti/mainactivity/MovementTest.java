379d38e5982d49750c50a924acff145f6cbb9cf9
Samuel <87152786+syntaxsavr@users.noreply.github.com>
2023-06-15 13:25:23 +0200

    Added Tests

--- app/src/test/java/com/example/lottikarotti/mainactivity/MovementTest.java
+++ app/src/test/java/com/example/lottikarotti/mainactivity/MovementTest.java
@@ -13,19 +13,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovementTest {
 
     private MockedStatic<ServerConnection> mockedServerConnection;
-    private String json;
+    private String playerstill, playermove;
 
     @BeforeEach
     public void setup() {
         // Mock the ServerConnection class
         mockedServerConnection = mockStatic(ServerConnection.class);
-        json = "[{\"sid\":\"LPRTFbJ099djPWfZAAAN\",\"lobbycode\":\"732836\",\"color\":\"white\",\"rabbits\":[{\"name\":\"rabbit1\",\"position\":0},{\"name\":\"rabbit2\",\"position\":0},{\"name\":\"rabbit3\",\"position\":0},{\"name\":\"rabbit4\",\"position\":0}]}]";
+        playerstill = "[{\"sid\":\"LPRTFbJ099djPWfZAAAN\",\"lobbycode\":\"732836\",\"color\":\"white\",\"rabbits\":[{\"name\":\"rabbit1\",\"position\":0},{\"name\":\"rabbit2\",\"position\":0},{\"name\":\"rabbit3\",\"position\":0},{\"name\":\"rabbit4\",\"position\":0}]}]";
+        playermove = "[{\"sid\":\"LPRTFbJ099djPWfZAAAN\",\"lobbycode\":\"732836\",\"color\":\"white\",\"rabbits\":[{\"name\":\"rabbit1\",\"position\":3},{\"name\":\"rabbit2\",\"position\":0},{\"name\":\"rabbit3\",\"position\":0},{\"name\":\"rabbit4\",\"position\":0}]}]";
 
     }
 
@@ -47,16 +49,28 @@ public class MovementTest {
     @Test
     public void successMovementTest() {
         Assertions.assertDoesNotThrow(() -> {
-            PlayerMove.handleMoveFromServer(json);
+            PlayerMove.handleMoveFromServer(playerstill);
         });
     }
 
     @Test
     public void handleIncomingMoveTest() throws JsonProcessingException {
         List<Player> players = new ArrayList<Player>();
-        players.add(new Player("LPRTFbJ099djPWfZAAAN", "732836", "white", null));
+        List<Player> playersServer = new ArrayList<Player>();
 
-        Assertions.assertNotEquals(PlayerMove.handleMoveFromServer(json).toArray().toString(), players.toArray().toString());
+        players.add(new Player("LPRTFbJ099djPWfZAAAN", "123456", "white", null));
+        playersServer = PlayerMove.handleMoveFromServer(playerstill);
+
+        Assertions.assertEquals(playersServer.get(0).getColor(), players.get(0).getColor());
+        Assertions.assertNotEquals(playersServer.get(0).getLobbycode(), players.get(0).getLobbycode());
+    }
+
+    @Test
+    public void testMovement() throws JsonProcessingException {
+        List<Player> players = new ArrayList<Player>();
+        List<Player> playersServer = new ArrayList<>();
+        players = PlayerMove.handleMoveFromServer(playerstill);
+
     }
 }
 
